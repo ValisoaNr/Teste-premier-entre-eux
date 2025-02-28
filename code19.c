27eb74b1d194972a246d32e1afb70eb46268a4d0
ValisoaNr <valisoandr12@gmail.com>
2025-02-28 09:13:18 +0300

    Update code19.c

--- code19.c
+++ code19.c
@@ -3,12 +3,10 @@
 int main ()
 {
 	int a , b , r ;
-	
 	printf("\tTESTER SI DEUX ENTIERS a ET b SONT PREMIER ENTRE EUX\nEntrez la valeur de a :");//Presentation du programme
 	scanf("%d", &a );
 	printf("Entrez la valeur de b :");
 	scanf("%d", &b );
-	
 	//premier entre eux si pgcd egale 1
 	do
 	{
@@ -16,18 +14,16 @@ int main ()
 		a = b ;
 		b = r ;
 		r = ( a % b );	
-	}while( r != 0 ) ; //Desormais, b est le dernier reste non nulle (le pgcd)
-	
+	}
+	while( r != 0 ) ; //Desormais, b est le dernier reste non nulle (le pgcd)
 	if( b == 1 )
 	{
 		printf("a et b sont premier entre eux\n");
 	}
-			
 		else
 		{
-		printf("a et b ne sont pas premier entre eux\n");
+			printf("a et b ne sont pas premier entre eux\n");
 		}
-		
 	return (0);
 }
 
